d0824055d9e6077711eb96d0b81cd88df3d5dfb3
VSRohod <89645335+VSRohod@users.noreply.github.com>
2024-02-07 17:15:36 -0300

    Upando arquivos

---
+++ PROGRAMAÇÃO/Lógica de programação/Lógica de programação - explore funções e listas/app.js
@@ -0,0 +1,23 @@
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
+
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Esolha um número entre 1 e 10';
+
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute(){
+    console.log('O botão foi clicado!');
+}
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * 10 + 1);
+}
