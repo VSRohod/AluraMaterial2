261db4d7b713b6b667375bf3baa916e5a9a1076d
VSRohod <89645335+VSRohod@users.noreply.github.com>
2024-02-07 10:58:11 -0300

    Lógica de programação - mergulhe em programação com JavaScript
    Upando arquivos

---
+++ PROGRAMAÇÃO/Lógica de programação/Lógica de programação - mergulhe em programação com JavaScript/app.js
@@ -0,0 +1,40 @@
+alert('Boas vindas ao jogo do número secreto');
+
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+
+let chute;
+let tentativas = 1;
+
+//enquanto chute não for igual ao numero secreto
+while(chute != numeroSecreto){
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // se chute for igual a numero secreto
+    if( numeroSecreto == chute){
+        break
+    } else {
+
+        if( numeroSecreto > chute){
+            alert(`Você errou! o número é menor que ${chute}`);
+        }else{
+            alert(`Você errou! o número é maior que ${chute}`);
+        }
+        tentativas++
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto! ${numeroSecreto} com ${tentativas} ${palavraTentativa} `);
+
+
+
+// if( tentativas > 1){
+//     alert(`Isso ai! Você descobriu o número secreto! ${numeroSecreto} com ${tentativas} tentativas `);
+// }else{
+//     alert(`Isso ai! Você descobriu o número secreto! ${numeroSecreto} com ${tentativas} tentativa `);
+// }
+
+//parseInt(Math.random() * 10)
+
+
