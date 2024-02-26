b9e79dabc5917cd3e1896929294c8949e4fe09e3
Victor Santos Rohod <victorsantosrohod@gmail.com>
2024-02-26 17:48:12 -0300

    Update app.js

--- PROGRAMAÇÃO/Lógica de programação/Lógica de programação - praticando com desafios/sorteador-numeros/app.js
+++ PROGRAMAÇÃO/Lógica de programação/Lógica de programação - praticando com desafios/sorteador-numeros/app.js
@@ -8,9 +8,17 @@ function sortear(){
     for (let i = 0; i < quantidade; i++){
         numero = obterNumeroAleatorio(de, ate)
         sorteados.push(numero);
+
+        while(sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(de, ate)
+        }
+
+        sorteados.push(numero)
     }
 
-    alert(sorteados);
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`
+    alterarStatusBotao();
 }
 
 
@@ -19,3 +27,22 @@ function obterNumeroAleatorio(min, max){
 }
 
 function reiniciar(){}
+
+function alterarStatusBotao(){
+   let botao = document.getElementById('btn-reiniciar')
+   if (botao.classList.contains('container__botao-desabilitado')) {
+    botao.classList.remove('container__botao-desabilitado');
+    botao.classList.add('container__botao');
+   } else {
+    botao.classList.remove('container__botao');
+    botao.classList.add('container__botao-desabilitado');
+   }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+}
